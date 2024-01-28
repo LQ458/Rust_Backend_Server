459ca589dac0e8dc9ff8f8666d60182e5f23db54
LQ <143152892+LQ458@users.noreply.github.com>
2024-01-28 19:19:23 +0800

    rust

---
+++ target/debug/deps/example.d
@@ -0,0 +1,5 @@
+D:\OneDrive\Documents\GitHub\Rust_Backend_Server\target\debug\deps\example.exe: src\main.rs
+
+D:\OneDrive\Documents\GitHub\Rust_Backend_Server\target\debug\deps\example.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/example.d
@@ -0,0 +1,1 @@
+D:\OneDrive\Documents\GitHub\Rust_Backend_Server\target\debug\example.exe: D:\OneDrive\Documents\GitHub\Rust_Backend_Server\src\main.rs
